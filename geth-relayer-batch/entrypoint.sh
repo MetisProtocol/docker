0bd12a900152779a181c2df26510e1a6a7f34374
guanjie <jguanisme@163.com>
2021-05-20 08:28:46 +0000

    fiexd: shell & Dockerfile modify

--- geth-relayer-batch/entrypoint.sh
+++ geth-relayer-batch/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "crontab starting..."
+service cron restart
+crontab -u root /app/crontab.key
 echo "supervisord starting..."
 nohup supervisord -c /etc/supervisor/conf.d/supervisord.conf>/app/log/t_supervisord.log &
 echo "log tail..."
